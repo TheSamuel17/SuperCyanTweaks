5c46e1d8de808f75d0336c4fba0194dd77c3dc31
TheSamuel17 <samuelseventeen17@gmail.com>
2026-01-14 04:08:42 -0500

    1.0.1 update (part 2)

---
+++ SuperCyanTweaks/Equipment/EccentricVase.cs
@@ -0,0 +1,104 @@
+﻿using MonoMod.Cil;
+using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace SuperCyanTweaks
+{
+    public class EccentricVase
+    {
+        public static BuffDef cloakBuff = Addressables.LoadAssetAsync<BuffDef>("RoR2/Base/Common/bdCloak.asset").WaitForCompletion();
+        public static BuffDef intangibleBuff = Addressables.LoadAssetAsync<BuffDef>("RoR2/Base/Common/bdIntangible.asset").WaitForCompletion();
+        public static BuffDef fallImmuneBuff = Addressables.LoadAssetAsync<BuffDef>("RoR2/Junk/Common/bdIgnoreFallDamage.asset").WaitForCompletion();
+        public static GameObject ziplineVehicle = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Gateway/ZiplineVehicle.prefab").WaitForCompletion();
+        public static EquipmentDef gateway = Addressables.LoadAssetAsync<EquipmentDef>("RoR2/Base/Gateway/Gateway.asset").WaitForCompletion();
+
+        public EccentricVase()
+        {
+            // Adjust cooldown
+            if (Configs.eccentricVaseCooldown.Value >= 0)
+            {
+                gateway.cooldown = Configs.eccentricVaseCooldown.Value;
+            }
+
+            // Adjust zipline length
+            if (Configs.eccentricVaseMaxDist.Value >= 0)
+            {
+                bool hookFailed = true;
+                IL.RoR2.EquipmentSlot.FireGateway += (il) =>
+                {
+                    ILCursor c = new(il);
+
+                    if (
+                        c.TryGotoNext(MoveType.Before,
+                        x => x.MatchLdcR4(1000f)
+                    ))
+                    {
+                        c.Next.Operand = Configs.eccentricVaseMaxDist.Value;
+                        hookFailed = false;
+                    }
+
+                    if (hookFailed == true)
+                    {
+                        Log.Error("Eccentric Vase length hook failed!");
+                    }
+                };
+
+                if (hookFailed == false)
+                {
+                    UpdateDescription(Configs.eccentricVaseMaxDist.Value);
+                }
+            }
+
+            // Adjust acceleration
+            if (Configs.eccentricVaseAcceleration.Value >= 0)
+            {
+                ziplineVehicle.GetComponent<ZiplineVehicle>().acceleration = Configs.eccentricVaseAcceleration.Value;
+            }
+
+            // Handle buffs while traveling
+            if (Configs.eccentricVaseCloak.Value == true || Configs.eccentricVaseIntangible.Value || Configs.eccentricVaseNoCrater.Value)
+            {
+                On.RoR2.ZiplineVehicle.OnPassengerEnter += OnPassengerEnter;
+                On.RoR2.ZiplineVehicle.OnPassengerExit += OnPassengerExit;
+            }
+        }
+
+        private void OnPassengerEnter(On.RoR2.ZiplineVehicle.orig_OnPassengerEnter orig, ZiplineVehicle self, GameObject passenger)
+        {
+            CharacterBody body = passenger.GetComponent<CharacterBody>();
+            if (body)
+            {
+                if (Configs.eccentricVaseCloak.Value == true) body.AddBuff(cloakBuff);
+                if (Configs.eccentricVaseIntangible.Value == true) body.AddBuff(intangibleBuff);
+            }
+
+            orig(self, passenger);
+        }
+
+        private void OnPassengerExit(On.RoR2.ZiplineVehicle.orig_OnPassengerExit orig, ZiplineVehicle self, GameObject passenger)
+        {
+            orig(self, passenger);
+
+            CharacterBody body = passenger.GetComponent<CharacterBody>();
+            if (body)
+            {
+                if (Configs.eccentricVaseCloak.Value == true) body.RemoveBuff(cloakBuff);
+                if (Configs.eccentricVaseIntangible.Value == true) body.RemoveBuff(intangibleBuff);
+                if (Configs.eccentricVaseNoCrater.Value == true) body.AddTimedBuff(fallImmuneBuff, .5f);
+            }
+        }
+
+        private void UpdateDescription(float distance)
+        {
+            string distanceString = distance.ToString().Replace(",", ".");
+
+            LanguageAPI.Add("EQUIPMENT_GATEWAY_DESC",
+                $"Create a <style=cIsUtility>quantum tunnel</style> of up to <style=cIsUtility>{distanceString}m</style> in length. " +
+                $"Lasts 30 seconds.",
+                "en"
+            );
+        }
+    }
+}
